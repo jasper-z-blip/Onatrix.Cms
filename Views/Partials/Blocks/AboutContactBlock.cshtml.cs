5dc6ba3281edb681054a52f5181c602bf4996734
Jasper <jaspersjohlen@live.se>
2025-12-05 18:29:16 +0100

    Refactor: Removed hardcoded sections and implemented Umbraco Block List elements across all pages

---
+++ Views/Partials/Blocks/AboutContactBlock.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Onatrix.Cms.Views.Partials.Blocks
+{
+    public class AboutContactBlockModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
